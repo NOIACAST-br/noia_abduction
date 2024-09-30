add612616983a84083b7777d07be7d402328c3c2
NOIACAST <lucasmongconann24@gmail.com>
2024-09-29 21:56:03 -0300

    config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.UfoSpawnZones = {
+    {id = 1, x = -1437.3301, y = -1413.6647, z= 92.4664, radius = 55.0 }, -- Exemplo de coordenadas e raio de spawn
+    -- Outras zonas
+}
+Config.UfoSpawnLimits = {
+    [1] = 1, -- Limite de OVNIs na zona 1
+    -- Outros limites
+}
+Config.NotificationCheckInterval = 1000 -- Intervalo para checar notificações de zonas
