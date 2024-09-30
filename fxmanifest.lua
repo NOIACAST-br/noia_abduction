cb0f153254724e9b5721cef49f257f6235c51f01
NOIACAST <lucasmongconann24@gmail.com>
2024-09-29 22:00:34 -0300

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'NOIACAST'
+description 'Noia Abdiction'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'ufo_client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+-- Aviso de pré-lançamento
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
