10a1a1f58f4f302c6d9167d6045e09f92c172bd3
NOIACAST <lucasmongconann24@gmail.com>
2024-10-03 21:39:23 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,8 +4,8 @@ Config.UfoSpawnZones = {
     {id = 1, x = -1437.3301, y = -1413.6647, z= 92.4664, radius = 55.0 }, -- Exemplo de coordenadas e raio de spawn
     -- Outras zonas
 }
-Config.UfoSpawnLimits = {
-    [1] = 1, -- Limite de OVNIs na zona 1
-    -- Outros limites
+Config.UfoSpawnInterval = 5000 -- Intervalo de 10 segundos, por exemplo
+Config.Messages = {
+    EnteredUfoZone = "Você entrou em uma zona de OVNIs.",
+    LeftUfoZone = "Você saiu da zona de OVNIs."
 }
-Config.NotificationCheckInterval = 1000 -- Intervalo para checar notificações de zonas
