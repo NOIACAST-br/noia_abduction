b3bf4b38cd9e1d2587c905676bbdcad989f79ffa
NOIACAST <lucasmongconann24@gmail.com>
2024-10-21 08:47:13 -0300

    Add files via upload

---
+++ languages/translation.lua
@@ -0,0 +1,15 @@
+Config.Messages = {
+    EnteredUfoZoneTitle = "Você entrou na zona de OVNI!",
+    EnteredUfoZoneSubtitle = "Cuidado, algo estranho está acontecendo.",
+    EnteredUfoZoneDict = "BLIPS",
+    EnteredUfoZoneIcon = "blip_ambient_eyewitness",
+    EnteredUfoZoneDuration = 5000,
+    EnteredUfoZoneColor = "COLOR_GREEN",
+
+    LeftUfoZoneTitle = "Você saiu da zona de OVNI.",
+    LeftUfoZoneSubtitle = "Parece que você está seguro agora.",
+    LeftUfoZoneDict = "BLIPS",
+    LeftUfoZoneIcon = "blip_ambient_eyewitness",
+    LeftUfoZoneDuration = 5000,
+    LeftUfoZoneColor = "COLOR_RED"
+}
